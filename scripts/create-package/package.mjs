48655fc1de49294c3df8860cc439e81261d82366
Nageshwar <nageshpawarpatil@gmail.com>
2026-06-27 01:08:13 +0530

    feat: add functions to check package existence and create package directory

---
+++ scripts/create-package/package.mjs
@@ -0,0 +1,35 @@
+import { access, mkdir } from "node:fs/promises";
+import { constants } from "node:fs";
+
+/* -------------------------------------------------------------------------- */
+/*                                   PACKAGE                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Checks whether a package already exists.
+ *
+ * @param {object} metadata
+ * @returns {Promise<boolean>}
+ */
+export async function checkPackageExists(metadata) {
+  try {
+    await access(metadata.packageDirectory, constants.F_OK);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+
+/**
+ * Creates the package directory.
+ *
+ * @param {object} metadata
+ * @returns {Promise<void>}
+ */
+export async function createPackageDirectory(metadata) {
+  await mkdir(metadata.packageDirectory, {
+    recursive: true,
+  });
+}
