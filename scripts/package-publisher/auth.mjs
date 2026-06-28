376a96e2ccf00f7a57ba635c3c0c4e723a8a97e9
Nageshwar <nageshpawarpatil@gmail.com>
2026-06-28 11:14:34 +0530

    Auth.mjs done

---
+++ scripts/package-publisher/auth.mjs
@@ -0,0 +1,31 @@
+import { whoami } from './npm.mjs';
+
+/* -------------------------------------------------------------------------- */
+/*                              AUTHENTICATION                                */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Ensures the user is logged in to npm.
+ *
+ * @returns {Promise<void>}
+ */
+export async function ensureLoggedIn() {
+  const username = await whoami();
+
+  if (!username) {
+    throw new Error('Please login to npm before continuing.');
+  }
+}
+
+/**
+ * Ensures the user is logged out of npm.
+ *
+ * @returns {Promise<void>}
+ */
+export async function ensureLoggedOut() {
+  const username = await whoami();
+
+  if (username) {
+    throw new Error(`Already logged in as "${username}".`);
+  }
+}
