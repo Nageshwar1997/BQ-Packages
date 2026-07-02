5aaed15376ecaf36ef654a6a1867651999e2c05d
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-03 02:30:30 +0530

    media-service types done

---
+++ packages/shared/types/src/media-service/image.ts
@@ -0,0 +1,4 @@
+import type { IMAGE_MIMES, VIDEO_MIMES } from '@beautinique/shared-constants';
+
+export type TImageMime = (typeof IMAGE_MIMES)[number];
+export type TVideoMime = (typeof VIDEO_MIMES)[number];
---
+++ packages/shared/types/src/media-service/video.ts
@@ -0,0 +1,4 @@
+import type { IMAGE_FORMATS, VIDEO_FORMATS } from '@beautinique/shared-constants';
+
+export type TImageFormat = (typeof IMAGE_FORMATS)[number];
+export type TVideoFormat = (typeof VIDEO_FORMATS)[number];
