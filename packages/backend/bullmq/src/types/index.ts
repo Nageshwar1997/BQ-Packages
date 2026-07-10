60408d92b2448b69e4f33994097e075a2e52c709
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-11 00:13:14 +0530

    feat(types): enhance job type definitions for better type safety

--- packages/backend/bullmq/src/types/index.ts
+++ packages/backend/bullmq/src/types/index.ts
@@ -1,3 +1,21 @@
+import type { JobsOptions } from 'bullmq';
+
+import type { QUEUE_SCHEMA } from '../constants/index.js';
+
+type TSchema = typeof QUEUE_SCHEMA;
+
+export type TQueueName = keyof TSchema;
+
+export type TJobName<Q extends TQueueName> = keyof TSchema[Q];
+
+export type TJobData<Q extends TQueueName, J extends TJobName<Q>> = TSchema[Q][J];
+
+export type TQueueJobUnion = {
+  [Q in TQueueName]: {
+    [J in TJobName<Q>]: { queueName: Q; jobName: J; data: TJobData<Q, J>; options?: JobsOptions };
+  }[TJobName<Q>];
+}[TQueueName];
+
 export interface TEmailOtp {
   email: string;
   otp: string;
