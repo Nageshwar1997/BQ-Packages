390c4c0504b24b65e00cbdbf92d1f3d3cfcbdf6e
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-15 02:05:16 +0530

    feat: update backend-zod to version 1.0.7 and shared-zod to version 1.0.2

--- packages/backend/zod/src/index.ts
+++ packages/backend/zod/src/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line simple-import-sort/exports
+export * from '@beautinique/shared-zod';
 export * from './media-service/index.js';
 export * from './middlewares/index.js';
 export type * from './types/index.js';
