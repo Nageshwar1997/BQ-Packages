dc268b5820b13d4a1345ac77e868086d354cb10e
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-17 16:30:41 +0530

    feat: add category Zod schemas for level 1, level 2, and level 3 categories, and update exports

--- packages/backend/zod/src/index.ts
+++ packages/backend/zod/src/index.ts
@@ -2,4 +2,5 @@
 export * from '@beautinique/shared-zod';
 export * from './media-service/index.js';
 export * from './middlewares/index.js';
+export * from './product-service/index.js';
 export type * from './types/index.js';
