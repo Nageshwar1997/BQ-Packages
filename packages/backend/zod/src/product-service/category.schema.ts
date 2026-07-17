3c86a4aed139f4e68bb83cb65122e112f81665e6
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-17 17:12:35 +0530

    chore: update shared-zod version to 1.0.5 and adjust dependencies in related packages

--- packages/backend/zod/src/product-service/category.schema.ts
+++ packages/backend/zod/src/product-service/category.schema.ts
@@ -1,40 +1,28 @@
 import { CATEGORY_LEVELS_MAP, REGEX } from '@beautinique/shared-constants';
-import { literal, object, string, union } from 'zod';
+import {
+  categoryLevelZodSchema,
+  categoryNameAndDescriptionZodSchema,
+} from '@beautinique/shared-zod';
+import { object, string, union } from 'zod';
 
 /* -------------------------------------------------------------------------- */
 /*                               COMMON SCHEMA                                */
 /* -------------------------------------------------------------------------- */
 
-const baseCategoryZodSchema = object({
-  name: string('Category name is required')
-    .trim()
-    .nonempty('Category name is required')
-    .min(2, 'Category name must be at least 2 characters long')
-    .max(120, 'Category name must be at most 120 characters long')
-    .regex(REGEX.SINGLE_SPACE, "Category name can't contain consecutive spaces"),
-  parent: string('Parent category is required')
-    .trim()
-    .nonempty('Parent category is required')
-    .length(24, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9')
-    .regex(REGEX.NO_SPACE, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9')
-    .regex(REGEX.MONGODB_ID, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9'),
-  description: string('Description is required')
-    .trim()
-    .nonempty('Description is required')
-    .min(10, 'Description must be at least 10 characters long')
-    .max(300, 'Description must be at most 300 characters long')
-    .regex(REGEX.SINGLE_SPACE, "Description can't contain consecutive spaces"),
-});
+const parentCategoryZodSchema = string('Parent category is required')
+  .trim()
+  .nonempty('Parent category is required')
+  .length(24, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9')
+  .regex(REGEX.NO_SPACE, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9')
+  .regex(REGEX.MONGODB_ID, 'Parent category is invalid. e.g. 63e5b8a5a4b0c2dabccce3f9');
 
 /* -------------------------------------------------------------------------- */
 /*                               LEVEL 1 SCHEMA                               */
 /* -------------------------------------------------------------------------- */
 
 export const l1CategoryZodSchema = object({
-  level: literal(CATEGORY_LEVELS_MAP.L1, {
-    message: `Category level must be ${String(CATEGORY_LEVELS_MAP.L1)}.`,
-  }),
-  name: baseCategoryZodSchema.shape.name,
+  level: categoryLevelZodSchema.shape[CATEGORY_LEVELS_MAP.L1],
+  name: categoryNameAndDescriptionZodSchema.shape.name,
 });
 
 /* -------------------------------------------------------------------------- */
@@ -42,11 +30,9 @@ export const l1CategoryZodSchema = object({
 /* -------------------------------------------------------------------------- */
 
 export const l2CategoryZodSchema = object({
-  level: literal(CATEGORY_LEVELS_MAP.L2, {
-    message: `Category level must be ${String(CATEGORY_LEVELS_MAP.L2)}.`,
-  }),
-  name: baseCategoryZodSchema.shape.name,
-  parent: baseCategoryZodSchema.shape.parent,
+  level: categoryLevelZodSchema.shape[CATEGORY_LEVELS_MAP.L2],
+  name: categoryNameAndDescriptionZodSchema.shape.name,
+  parent: parentCategoryZodSchema,
 });
 
 /* -------------------------------------------------------------------------- */
@@ -54,12 +40,10 @@ export const l2CategoryZodSchema = object({
 /* -------------------------------------------------------------------------- */
 
 export const l3CategoryZodSchema = object({
-  level: literal(CATEGORY_LEVELS_MAP.L3, {
-    message: `Category level must be ${String(CATEGORY_LEVELS_MAP.L3)}.`,
-  }),
-  name: baseCategoryZodSchema.shape.name,
-  parent: baseCategoryZodSchema.shape.parent,
-  description: baseCategoryZodSchema.shape.description,
+  level: categoryLevelZodSchema.shape[CATEGORY_LEVELS_MAP.L3],
+  name: categoryNameAndDescriptionZodSchema.shape.name,
+  parent: parentCategoryZodSchema,
+  description: categoryNameAndDescriptionZodSchema.shape.description,
 });
 
 /* -------------------------------------------------------------------------- */
