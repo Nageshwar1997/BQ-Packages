1a0c41d183fdd51f03c712941f5b7ac14eff719c
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-19 02:28:31 +0530

    feat: update shared dependencies to version 1.0.7 and add new Zod schemas for images and videos

--- packages/shared/types/src/common/zodSchema.types.ts
+++ packages/shared/types/src/common/zodSchema.types.ts
@@ -1,10 +1,13 @@
 import type {
   confirmPasswordZodSchema,
   emailZodSchema,
+  imageUnionZodSchema,
   infer as Infer,
   otpZodSchema,
   passwordZodSchema,
   phoneNumberZodSchema,
+  thumbnailUnionZodSchema,
+  videoUnionZodSchema,
 } from '@beautinique/shared-zod';
 
 export type TEmailZodSchema = Infer<typeof emailZodSchema>;
@@ -16,3 +19,9 @@ export type TPasswordZodSchema = Infer<typeof passwordZodSchema>;
 export type TConfirmPasswordZodSchema = Infer<typeof confirmPasswordZodSchema>;
 
 export type TOtpZodSchema = Infer<typeof otpZodSchema>;
+
+export type TThumbnailUnionZodSchema = Infer<typeof thumbnailUnionZodSchema>;
+
+export type TVideoUnionZodSchema = Infer<typeof videoUnionZodSchema>;
+
+export type TImageUnionZodSchema = Infer<typeof imageUnionZodSchema>;
