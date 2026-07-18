2eec65b74cd70839eb94ac626995c827f9691b56
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-19 03:40:12 +0530

    feat: refactor product service types by consolidating Zod schemas and adding category types

--- packages/backend/types/src/product-service/index.ts
+++ packages/backend/types/src/product-service/index.ts
@@ -1,15 +1,1 @@
-import type {
-  categoryUpdateZodSchema,
-  categoryZodSchema,
-  l1CategoryZodSchema,
-  l2CategoryZodSchema,
-  l3CategoryZodSchema,
-  TInfer,
-} from '@beautinique/backend-zod';
-
-export type TL1CategoryZodSchema = TInfer<typeof l1CategoryZodSchema>;
-export type TL2CategoryZodSchema = TInfer<typeof l2CategoryZodSchema>;
-export type TL3CategoryZodSchema = TInfer<typeof l3CategoryZodSchema>;
-
-export type TCategoryZodSchema = TInfer<typeof categoryZodSchema>;
-export type TCategoryUpdateZodSchema = TInfer<typeof categoryUpdateZodSchema>;
+export * from './zodSchema.types.js';
