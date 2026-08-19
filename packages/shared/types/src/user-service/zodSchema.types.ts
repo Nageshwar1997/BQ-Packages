1095871f715215a21b100e8818ff14c71cbd0038
Nageshwar <nageshpawarpatil@gmail.com>
2026-08-19 13:52:18 +0530

    types republished

--- packages/shared/types/src/user-service/zodSchema.types.ts
+++ packages/shared/types/src/user-service/zodSchema.types.ts
@@ -1,10 +1,12 @@
 import type {
+  assignAdminTerritoryZodSchema,
   changePasswordZodSchema,
   loginZodSchema,
   passwordsZodSchema,
   registerZodSchema,
   setPasswordZodSchema,
   TInfer,
+  updateAdminStatusZodSchema,
 } from '@beautinique/shared-zod';
 
 export type TLoginZodSchema = TInfer<typeof loginZodSchema>;
@@ -16,3 +18,7 @@ export type TRegisterZodSchema = TInfer<typeof registerZodSchema>;
 export type TSetPasswordZodSchema = TInfer<typeof setPasswordZodSchema>;
 
 export type TChangePasswordZodSchema = TInfer<typeof changePasswordZodSchema>;
+
+export type TAssignAdminTerritoryZodSchema = TInfer<typeof assignAdminTerritoryZodSchema>;
+
+export type TUpdateAdminStatusZodSchema = TInfer<typeof updateAdminStatusZodSchema>;
